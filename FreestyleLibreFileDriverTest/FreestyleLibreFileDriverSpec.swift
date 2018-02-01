5cd41ef108d08cf01c4822bd926c00d0ffc15662
Thomas Bonk <thomas@meandmymac.de>
2018-02-01 20:21:14 +0100

    - added dummy test case - changed travis configuration

---
+++ FreestyleLibreFileDriverTest/FreestyleLibreFileDriverSpec.swift
@@ -0,0 +1,23 @@
+//
+//  FreestyleLibreFileDriverSpec.swift
+//  FreestyleLibreFileDriverTest
+//
+//  Created by Thomas Bonk on 01.02.18.
+//
+
+import Quick
+import Nimble
+
+class FreestyleLibreFileDriverSpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("FreestyleLibreFileDriverSpec") {
+            it("works") {
+            }
+        }
+        
+    }
+    
+}
+
